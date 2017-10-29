2775c02741d9c9b471ca6046f1eed033638bef82
sampoyigi <sam@sampoyigi.com>
2017-10-29 16:07:08 +0100

    Major changes for v3, using frontmatter for pages, render components directly in page

---
+++ _pages/home.php
@@ -0,0 +1,35 @@
+---
+title: main::home.text_heading
+layout: default
+permalink: /
+
+'[slider]':
+
+'[localSearch]':
+---
+<?
+function onInit()
+{
+}
+
+function onStart()
+{
+}
+
+function onEnd()
+{
+}
+
+?>
+---
+<?= component('slider'); ?>
+
+<?= component('localSearch'); ?>
+
+<div id="page-content">
+    <div class="container">
+        <div class="content-wrap">
+            <?= partial('cta/home'); ?>
+        </div>
+    </div>
+</div>
