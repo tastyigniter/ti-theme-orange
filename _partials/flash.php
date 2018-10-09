4696b3b9e01239f3be14b0af708d1bb94edd7653
SamPoyigi <sam@sampoyigi.com>
2018-10-09 23:23:21 +0100

    Fix flash message issue

--- _partials/flash.php
+++ _partials/flash.php
@@ -1,39 +1,20 @@
 <?php foreach (Flash::all() as $message) { ?>
     <?php if ($message['overlay']) { ?>
         <div
-            class="modal fade flash-modal"
-            data-control="flash-modal"
-        >
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title"><?= $message['title'] ?></h4>
-                    </div>
-
-                    <div class="modal-body">
-                        <p><?= $message['message'] ?></p>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+            data-control="flash-overlay"
+            data-title="<?= array_get($message, 'title') ?>"
+            data-text="<?= array_get($message, 'message') ?>"
+            data-icon="<?= $message['level']; ?>"
+            data-close-on-click-outside="<?= $message['important'] ? 'false' : 'true'; ?>"
+            data-close-on-esc="<?= $message['important'] ? 'false' : 'true'; ?>"
+        ></div>
     <?php }
     else { ?>
-        <div class="alert alert-<?= $message['level']; ?><?= $message['important'] ? ' alert-important' : ''; ?>"
-             role="alert"
-        >
-            <!--            --><?php //if ($message['important']) { ?>
-            <button type="button"
-                    class="close"
-                    data-dismiss="alert"
-                    aria-hidden="true"
-            >&times;</button>
-            <!--            --><?php //} ?>
-            <?= $message['message']; ?>
-        </div>
+        <div
+            class="alert alert-<?= $message['level']; ?><?= $message['important'] ? ' alert-important' : ''; ?>"
+            data-control="flash-message"
+            data-allow-dismiss="<?= $message['important'] ? 'false' : 'true'; ?>"
+            role="alert"
+        ><?= $message['message']; ?></div>
     <?php } ?>
 <?php } ?>
