046caee7360bdcb9a2994c106fd1dd62a0d6e6e3
SamPoyigi <sam@sampoyigi.com>
2019-07-27 11:42:31 +0100

    Minor improvements to styling

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -34,44 +34,6 @@ $(function () {
 })
 
 $(function () {
-    var alertMsgs = $('.alert-collapsible .alert-hide')
-    var dropdownButton = $('.btn-dropdown')
-    alertMsgs.hide()
-
-    //Click dropdown
-    dropdownButton.click(function () {
-        //get data-for attribute
-        //var dataFor = $(this).attr('data-for');
-        //var idFor = $(dataFor);
-
-        //current button
-        var currentButton = $(this)
-        alertMsgs.slideToggle(function () {
-            //Completed slidetoggle
-            if (alertMsgs.is(':visible')) {
-                currentButton.html('<i class="fa fa-chevron-up text-muted"></i>')
-            } else {
-                currentButton.html('<i class="fa fa-chevron-down text-muted"></i>')
-            }
-        })
-    })
-})
-
-$(function () {
-
-    // $(document).on('change', '.btn-group-toggle input[type="radio"], .btn-group input[type="radio"]', function () {
-    //     var btn = $(this).parent()
-    //     var parent = btn.parent()
-    //     var activeClass = (btn.attr('data-btn')) ? btn.attr('data-btn') : 'btn-default'
-    //
-    //     parent.find('.btn').each(function () {
-    //         removeClass = ($(this).attr('data-btn')) ? $(this).attr('data-btn') : activeClass
-    //         $(this).removeClass(removeClass)
-    //     })
-    //
-    //     btn.addClass(activeClass)
-    // })
-
     $('.button-checkbox').each(function () {
 
         // Settings
@@ -115,8 +77,7 @@ $(function () {
                 $button
                     .removeClass('btn-default')
                     .addClass('btn-' + color + ' active')
-            }
-            else {
+            } else {
                 $button
                     .removeClass('btn-' + color + ' active')
                     .addClass('btn-default')
@@ -138,26 +99,6 @@ $(function () {
     })
 })
 
-function displayRatings(ratings) {
-    $('.rating-star').raty({
-        score: function () {
-            return $(this).attr('data-score')
-        },
-        scoreName: function () {
-            return $(this).attr('data-score-name')
-        },
-        readOnly: function () {
-            return $(this).attr('data-readonly') == 'true'
-        },
-        hints: ratings,
-        starOff: 'fa fa-star-o',
-        starOn: 'fa fa-star',
-        cancel: false, half: false, starType: 'i'
-    })
-
-    $('.rating-star i[title]').tooltip({placement: 'bottom'})
-}
-
 $(function () {
     $(window).bind("load resize", function () {
         $('.affix-module').each(function () {
@@ -175,7 +116,7 @@ $(function () {
         cookieValue = 'on',
         cookieDuration = 30
 
-    if (options.active === "1") {
+    if (options.active === 1) {
         if (checkCookie(cookieName) !== cookieValue) {
             $el.fadeIn()
         }
