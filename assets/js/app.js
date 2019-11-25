47e99c309d8dc9eb614d1e8349c345c7ce029ef8
SamPoyigi <sam@sampoyigi.com>
2019-11-25 22:49:28 +0000

    Minor ui improvements

--- assets/js/app.js
+++ assets/js/app.js
@@ -455,18 +455,6 @@ $(this).show()
 $(this).first().click()}
 $(function(){$(window).on('ajaxErrorMessage',function(event,message){event.preventDefault()
 $.ti.flashMessage({class:'danger',text:message})})})
-$(function(){$('.button-checkbox').each(function(){var $widget=$(this),$button=$widget.find('button'),$checkbox=$widget.find('input:checkbox'),color=$button.data('color'),settings={on:{icon:'fa fa-check-square-o'},off:{icon:'fa fa-square-o'}}
-$button.on('click',function(){$checkbox.prop('checked',!$checkbox.is(':checked'))
-$checkbox.triggerHandler('change')
-updateDisplay()})
-$checkbox.on('change',function(){updateDisplay()})
-function updateDisplay(){var isChecked=$checkbox.is(':checked')
-$button.data('state',(isChecked)?"on":"off")
-$button.find('.state-icon').removeClass().addClass('state-icon '+settings[$button.data('state')].icon)
-if(isChecked){$button.removeClass('btn-default').addClass('btn-'+color+' active')}else{$button.removeClass('btn-'+color+' active').addClass('btn-default')}}
-function init(){updateDisplay()
-if($button.find('.state-icon').length==0){$button.prepend('<i class="state-icon '+settings[$button.data('state')].icon+'"></i>')}}
-init()})})
 $(function(){$(window).bind("load resize",function(){$('.affix-module').each(function(){$(this).find('[data-spy="affix"]:first-child').css('width',$(this).width())})
 $('body').css({'padding-bottom':$('.footer').outerHeight()+10+'px'});})
 $('body').css({'padding-bottom':$('.footer').outerHeight()+10+'px'});})
