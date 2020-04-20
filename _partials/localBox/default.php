456c41aea76f88c8865065b0d51a6aeeb780d4b5
olieydt <olieydt@hotmail.com>
2020-04-19 22:26:30 -0400

    moving time data to timeslot

--- _partials/localBox/default.php
+++ _partials/localBox/default.php
@@ -3,8 +3,12 @@
         <div class="panel-body">
             <div class="row">
                 <div class="col-sm-<?= ($hideSearch) ? '12' : '4' ?> mb-3 mb-sm-0">
-                    <?= partial('@timeslot'); ?>
-                </div>
+		    <div
+                       id="local-timeslot"
+                    >
+                    	<?= partial('@timeslot'); ?>
+                    </div>
+		</div>
                 <?php if (!$hideSearch) { ?>
                     <div class="col-sm-8">
                         <?= partial('@searchbar'); ?>
