237faa7b084014303eb0d0e3672583b24525a352
Ryan Mitchell <ryan@thoughtcollective.com>
2020-05-06 19:17:32 +0100

    Add necessary vars to update modal price

--- _partials/cartBox/item_modal.php
+++ _partials/cartBox/item_modal.php
@@ -1,7 +1,9 @@
 <div
   class="modal-dialog "
   data-control="cart-item"
-  data-min-quantity="<?= $menuItem->minimum_qty; ?>"
+  data-min-quantity="<?= $menuItem->minimum_qty; ?>" 
+  data-price="<?= $cartItem ? $cartItem->price : $menuItem->getBuyablePrice() ?>"
+  data-price-format="<?= currency_format(0); ?>"
 >
   <form method="POST" data-request="<?= $formHandler; ?>">
     <div class="modal-content">
@@ -78,7 +80,7 @@
                 ? lang('igniter.cart::default.button_update')
                 : lang('igniter.cart::default.button_add_to_order');
               ?>
-              <span class="pull-right">
+              <span class="pull-right" data-output="lineprice">
                 <?= currency_format($cartItem
                   ? $cartItem->subtotal
                   : $menuItem->getBuyablePrice());
