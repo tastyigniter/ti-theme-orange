7af0720e9a76d119539891cf037851153bd50e60
Sam Poyigi <sam@sampoyigi.com>
2020-05-18 00:17:05 +0100

    Use language string for + and -

--- _partials/cartBox/item_option_checkbox.php
+++ _partials/cartBox/item_option_checkbox.php
@@ -20,7 +20,9 @@
             for="menuOptionCheck<?= $optionIndex; ?>"
         >
             <?= $optionValue->name; ?>
-            <?php if ($optionValue->price > 0 || !$hideZeroOptionPrices) {?><span class="pull-right">+<?= currency_format($optionValue->price); ?></span><?php } ?>
+            <?php if ($optionValue->price > 0 || !$hideZeroOptionPrices) {?>
+                <span class="pull-right"><?= lang('main::lang.text_plus').currency_format($optionValue->price); ?></span>
+            <?php } ?>
         </label>
     </div>
 <?php } ?>
