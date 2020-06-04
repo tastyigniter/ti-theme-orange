c1b18a7cdbdd4c4d5b47076605b1d38d69f2b98b
Ryan Mitchell <ryan@thoughtcollective.com>
2020-06-04 14:56:42 +0100

    HTML and style changes
    I tried to regenerate app.scss but I lost the font icons in doing so, so didn't commit it

--- _partials/cartBox/tip_form.php
+++ _partials/cartBox/tip_form.php
@@ -6,6 +6,7 @@
         data-request="<?= $applyTipEventHandler; ?>"
     >
         <div class="cart-tip">
+<<<<<<< HEAD
             <?php
             $tip = $cart->getCondition('tip');
             $tipAmountType = $tip ? $tip->getMetaData('amountType', 'none') : 'none';
@@ -57,6 +58,27 @@
                         title="<?= lang('igniter.cart::default.button_apply_tip'); ?>"
                     ><i class="fa fa-check"></i></button>
                 </div>
+=======
+            <div
+                class="input-group">
+                <input
+                    type="text"
+                    name="tip_amount"
+                    class="form-control"
+                    value="<?= ($coupon = $cart->getCondition('coupon')) ? $coupon->getMetaData('code') : '' ?>"
+                    placeholder="<?= lang('igniter.cart::default.text_apply_tip'); ?>"
+                	inputmode="numeric" 
+                	pattern="[0-9\.]*"    
+                />
+                <span class="input-group-append">
+                <button
+                    type="submit"
+                    class="btn btn-light"
+                    data-replace-loading="fa fa-spinner fa-spin"
+                    title="<?= lang('igniter.cart::default.button_apply_tip'); ?>"
+                ><i class="fa fa-check"></i></button>
+            </span>
+>>>>>>> HTML and style changes
             </div>
         </div>
     </form>
