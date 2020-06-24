920117c5a92cff32dd3a1c589b05bb0b5b4d43c8
Serios <seriosbg@gmail.com>
2020-06-24 10:07:41 +0300

    Fixes issue with option or quantity change causing wrong number of decimals
    Commit #70ad05d on tastyigniter/ti-ext-cart doesn't consider different currency symbol position than in front of the price amount when calculating decimal length, but if the currency symbol is after the price amount, the decimal length is calculated wrongly, counting currency symbol too. This fixes it by not including currency symbol in price-format data

--- _partials/cartBox/item_modal.php
+++ _partials/cartBox/item_modal.php
@@ -3,7 +3,7 @@
   data-control="cart-item"
   data-min-quantity="<?= $menuItem->minimum_qty; ?>" 
   data-price-amount="<?= $cartItem ? $cartItem->price : $menuItem->getBuyablePrice() ?>"
-  data-price-format="<?= currency_format(0); ?>"
+  data-price-format="<?= currency_format(0, 0, False); ?>"
 >
   <form method="POST" data-request="<?= $formHandler; ?>">
     <div class="modal-content">
