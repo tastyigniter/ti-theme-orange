98f86ee865f411d3f37499d41203d35227b4cb58
Mauricio Lattke <babel.hds@gmail.com>
2020-08-28 12:34:33 -0400

    Allergens UI

--- _partials/localMenu/item.php
+++ _partials/localMenu/item.php
@@ -6,6 +6,7 @@ $specialActive = ($special AND $special->active());
 $menuHasOptions = $menuItem->hasOptions();
 $menuPrice = $specialActive ? $special->getMenuPrice($menuItem->menu_price) : $menuItem->menu_price;
 $mealtimeTitles = [];
+$allergens = $menuItem->allergens;
 foreach ($menuItem->mealtimes ?? [] as $mealtime) {
     $mealtimeTitles[] = sprintf(lang('igniter.local::default.text_mealtime'),
         $mealtime->mealtime_name, $mealtime->start_time, $mealtime->end_time
@@ -70,6 +71,25 @@ foreach ($menuItem->mealtimes ?? [] as $mealtime) {
                     <i class="fa fa-<?= $mealtimeNotAvailable ? 'clock-o' : 'plus' ?>"></i>
                 </button>
             </span>
-        </div>
+        </div>            
     </div>
+        <div class="d-flex flex-row allergens">                
+            <?php foreach ($allergens as $allergen): ?>
+                <?php if($allergen->status && $allergen->hasMedia('thumb')) { ?>
+                    <img
+                        class="img-responsive img-rounded"
+                        alt="<?= $allergen->name; ?>"
+                        src="<?= $allergen->getThumb() ?>"                        
+                        data-toggle="tooltip" 
+                        title="<?= $allergen->name ?>: <?= $allergen->description ?>"
+                    >
+                <?php } elseif ($allergen->status) {?> 
+                    <button class="btn btn-primary btn-sm mx-1"
+                            data-toggle="tooltip" 
+                            title="<?= $allergen->description ?>">
+                        <?= $allergen->name ?>
+                    </button>
+                <?php } ?>                       
+            <?php endforeach; ?>
+        </div>
 </div>
