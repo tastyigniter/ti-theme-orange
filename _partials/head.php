432f580fa6c9989dad1bb5c9b260594246fbf7bc
Brad Pokroy <69753676+bradpokroy@users.noreply.github.com>
2020-09-20 21:39:36 +1000

    Update head.php

--- _partials/head.php
+++ _partials/head.php
@@ -3,21 +3,15 @@ description: ''
 ---
 <?= get_metas(); ?>
 <meta name="csrf-token" content="<?= csrf_token() ?>">
-<!--<script async src="https://www.googletagmanager.com/gtag/js?id=G-4459SWSTHD"></script>-->
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4459SWSTHD"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-4459SWSTHD');
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=<?= setting('maps_api_key'); ?>&language=<?= setting('default_language'); ?>&region=AU&libraries=places"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-4459SWSTHD"></script>
-<!--
-<script src="https://maps.googleapis.com/maps/api/js?location=25.2744, 133.7751&libraries=places&key=AIzaSyDaCODPfbEJq7MJ3mCxka8yts0SDHUTv-8&libraries=places&callback=initAutocomplete" async defer></script>
--->
+
 <script src="/themes/tastyigniter-orange-ddv/assets/js/smoova.js"></script>
 <link rel="stylesheet" href="/themes/tastyigniter-orange-ddv/assets/css/smoova.css">
 
