b58183653ba697786e51c50afedac74e21047743
Sam Poyigi <sam@sampoyigi.com>
2020-11-02 14:10:27 +0000

    Reference google font using link tag instead of @import

--- _partials/head.php
+++ _partials/head.php
@@ -7,6 +7,7 @@ else { ?>
     <?= get_favicon(); ?>
 <?php } ?>
 <title><?= sprintf(lang('main::lang.site_title'), lang(get_title()), setting('site_name')); ?></title>
+<link href="//fonts.googleapis.com/css?family=Amaranth|Titillium+Web:200,200i,400,400i,600,600i,700,700i|Droid+Sans+Mono" rel="stylesheet">
 <?= get_style_tags(); ?>
 <?php if (!empty($this->theme->custom_css)) { ?>
     <style type="text/css" id="custom-css"><?= $this->theme->custom_css; ?></style>
