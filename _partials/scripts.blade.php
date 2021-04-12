16fb7947126bc4d72bd73d3779aa29d8e5fe59c8
Sam Poyigi <sam@sampoyigi.com>
2021-04-12 11:06:56 +0100

    Use blade syntax

--- _partials/scripts.blade.php
+++ _partials/scripts.blade.php
@@ -1,5 +1,5 @@
 {!! Assets::getJsVars() !!}
-{!! get_script_tags() !!}
+@scripts
 {!! $this->theme->ga_tracking_code !!}
 @if (!empty($this->theme->custom_js))
     <script type="text/javascript">{!! $this->theme->custom_js !!}</script>
