7a69bbd12a05b51d4fce0204ce261e02ddb8b251
Sam Poyigi <sam@sampoyigi.com>
2021-04-14 00:19:49 +0100

    Add missing meta description and keywords tag #27

--- _partials/head.blade.php
+++ _partials/head.blade.php
@@ -6,6 +6,8 @@
     {!! get_favicon() !!}
 @endif
 <title>{{ sprintf(lang('main::lang.site_title'), lang(get_title()), setting('site_name')) }}</title>
+@if ($this->page->description)<meta name="description" content="{{ $this->page->description }}">@endif
+@if ($this->page->keywords)<meta name="keywords" content="{{ $this->page->keywords }}">@endif
 <link href="//fonts.googleapis.com/css?family=Amaranth|Titillium+Web:200,200i,400,400i,600,600i,700,700i|Droid+Sans+Mono" rel="stylesheet">
 @styles
 @if (!empty($this->theme->custom_css))
