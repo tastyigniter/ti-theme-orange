507338d0d057fcac311d016e51f5a577b2222bd1
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-04-24 15:42:25 +0100

    Bring back version constraints to composer deps
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- extensions/igniter/frontend/components/banners/default.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="module-box">
-    @if ($banner)
-        @if ($banner->isCustom)
-            {!! $banner->value !!}
-        @else
-            @partial('@images', ['banner' => $banner])
-        @endif
-    @endif
-</div>
--- extensions/igniter/socialite/language/en/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'text_confirm_email' => 'Confirm your email address',
-
-    'button_confirm' => 'Confirm',
-
-    'help_confirm_email' => '',
-];
