8a4936a0772d0c72e81199f831d363a464dc2913
sampoyigi <6567634+sampoyigi@users.noreply.github.com>
2026-06-14 14:45:13 +0100

    test(middleware): disable `CheckInitialSetup` middleware in auth-related tests

--- tests/Http/Controllers/LogoutTest.php
+++ tests/Http/Controllers/LogoutTest.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace Igniter\Orange\Tests\Http\Controllers;
 
+use Igniter\Main\Http\Middleware\CheckInitialSetup;
 use Igniter\User\Facades\Auth;
 use Igniter\User\Models\Customer;
 
 it('logs out the user', function(): void {
     $customer = Customer::factory()->create();
 
-    $this->actingAs($customer)->get(route('igniter.theme.account.logout'));
+    $this
+        ->withoutMiddleware(CheckInitialSetup::class)
+        ->actingAs($customer)
+        ->get(route('igniter.theme.account.logout'));
 
     expect(Auth::check())->toBeFalse();
 });
